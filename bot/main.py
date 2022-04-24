a2ab0b0133ca1cc7adac1ac0e3551fd2c095b416
LeudoNeto <leudoneto1234@gmail.com>
2022-04-24 15:57:23 -0300

    tirando sistema de mais de um prefixo, só condunde

--- bot/main.py
+++ bot/main.py
@@ -1,28 +1,11 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
-import os
-from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.members = True
 
-#Baseado em https://gist.github.com/EvieePy/d78c061a4798ae81be9825468fe146be
-def get_prefix(bot, message):
-    """Lista com mais de um prefixo possível por servidor."""
-
-    #Você também pode usar espaços nos prefixos
-    prefixes = ['ns2', 'nezuko ', '+']
-
-    #Checa se a mensagem foi no server ou no direct.
-    if not message.guild:
-        #Permite apenas que o prefixo '+' seja usado nos direct.
-        return '+'
-
-    # Se foi no server, permite que o usuário mencione o bot ou use algum dos prefixos dentro da lista.
-    return commands.when_mentioned_or(*prefixes)(bot, message)
-
-bot = commands.Bot(command_prefix=get_prefix, intents=intents, allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user=True))
+bot = commands.Bot(command_prefix="+", intents=intents, allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user=True))
 bot.launch_time = datetime.utcnow()
 
 bot.load_extension('music')
