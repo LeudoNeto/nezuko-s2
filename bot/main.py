dca9c1163a823df203e3f51a0f1971b2c30b5ac0
LeudoNeto <leudoneto1234@gmail.com>
2022-05-04 12:01:43 -0300

    preparando sistema de prefixo por server

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 #pip install -U git+https://github.com/Rapptz/discord.py    no terminal
 import discord
 from discord.ext import commands
+from config import bot_prefix
 
 intents = discord.Intents.all()
 
@@ -11,7 +12,7 @@ class MyBot(commands.Bot):
         await self.load_extension('funny')
         await self.load_extension('help')
 
-bot = MyBot(command_prefix="+", intents=intents, help_command=None, allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user=True))
+bot = MyBot(command_prefix=bot_prefix, intents=intents, help_command=None, allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False, replied_user=True))
 
 #on_ready: Quando ativo e funcional, retornará mensagem falando que foi conectado
 @bot.event
